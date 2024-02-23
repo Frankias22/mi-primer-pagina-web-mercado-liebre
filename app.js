909c9e360cfabcff390463fecfc10ffaa8afa6f4
Frankias22 <franco.aguilar598@gmail.com>
2024-02-23 11:21:41 -0300

    refactorizacion completa del sitio

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const app = express()
-const path = require('path')
+const express = require("express");
+const app = express();
 
-app.use(express.static(path.resolve(__dirname,'./public')))
+const path = require ("path");
 
-app.listen (3000,()=>{
+const publicPath = path.resolve(__dirname , "./public");
 
-})
+app.use (express.static (publicPath));
 
-app.get('/',(req,res) =>{
-    res.sendFile(path.resolve(__dirname,'./views/home.html'))
-})
-console.log();
+app.listen (3000, () => 
+    console.log("Servidor corriendo en Puerto 3000")
+);
+
+app.get("/", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+});
+
